8789c3ae8000135be6b9444e9e596331625a9ded
viral0310 <viraltada2001@gmail.com>
2023-07-04 13:21:12 +0530

    main

--- lib/Screens/PhoneScreen.dart
+++ lib/Screens/PhoneScreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../utils.dart';
-import 'Verificationscreen.dart';
+import 'VerificationScreen.dart';
 
 class PhoneScreen extends StatefulWidget {
   const PhoneScreen({super.key});
